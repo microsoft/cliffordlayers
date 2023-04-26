366b8ec3c04e1c7cc53b5a3d93a97f93f12328a1
rejuvyesh <mail@rejuvyesh.com>
2023-04-26 23:23:46 +0000

    reexport modules under nn

---
+++ cliffordlayers/nn/__init__.py
@@ -0,0 +1,3 @@
+from .modules.cliffordconv import CliffordConv1d, CliffordConv2d, CliffordConv3d
+from .modules.cliffordlinear import CliffordLinear
+from .modules.cliffordfourier import CliffordSpectralConv2d, CliffordSpectralConv3d
